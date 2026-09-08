7b834682dd5d503bbf535b1f726239800bf54847
Anloren21 <anlorerohu1996@gmail.com>
2026-09-08 11:41:52 +0200

    - Valida el stock antes de insertar una venta - Cancela ventas sin existencias suficientes - Descuenta automáticamente el stock tras una venta válida

---
+++ SQL_relaciones/tg_control_stock.sql
@@ -0,0 +1,59 @@
+DELIMITER //
+
+CREATE TRIGGER tg_control_stock
+BEFORE INSERT ON ventas 
+FOR EACH ROW
+BEGIN
+    DECLARE v_stock INT;
+
+    SELECT p.stock
+    INTO v_stock
+    FROM productos p
+    WHERE p.id = NEW.producto_id;
+
+    IF NEW.cantidad > v_stock THEN
+
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Stock insuficiente';
+
+    ELSE
+
+        UPDATE productos
+        SET stock = stock - NEW.cantidad
+        WHERE id = NEW.producto_id;
+
+    END IF;
+
+END 
+
+DELIMITER ;
+
+SELECT p.id, p.nombre, p.stock
+FROM productos p;
+
+DELIMITER ;
+
+INSERT INTO ventas (producto_id, cantidad)
+VALUES (13,10);
+
+DELIMITER ;
+
+SELECT
+    v.id,
+    p.nombre AS producto,
+    v.cantidad,
+    v.fecha
+FROM ventas v
+JOIN productos p
+    ON p.id = v.producto_id;
+
+SELECT
+    v.id,
+    p.nombre AS producto,
+    CONCAT(FORMAT(p.precio, 2), ' €') AS precio_unitario,
+    v.cantidad,
+    CONCAT(FORMAT(p.precio * v.cantidad, 2), ' €') AS total,
+    v.fecha
+FROM ventas v
+JOIN productos p
+    ON p.id = v.producto_id;
